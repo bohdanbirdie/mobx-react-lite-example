4bd7ab76d389a1382a46ad37fd6d441302c0e2a0
Bohdan Ptyts <voja4ok@yahoo.com>
2020-09-22 23:54:58 +0300

    Add App.tsx tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { useStore as storeHook } from "./hooks";
 import App from "./App";
 import { CounterStore } from "./CounterStore";
 
 const useStore = storeHook as ReturnType<typeof jest["fn"]>;
-jest.mock("hooks/store");
+jest.mock("./hooks");
 
 const stubCounterStore = () => {
   const store = new CounterStore();
-
-  store.value = 10;
 
   return store;
 };
@@ -23,5 +22,41 @@ describe("FilterComponent", () => {
 
   it("it renders without error", () => {
     expect(render(<App />)).toBeTruthy();
+  });
+
+  it("it renders initial counter value different from 0", () => {
+    const store = stubCounterStore();
+    store.value = 10;
+    useStore.mockReturnValue(store);
+
+    const { queryByText } = render(<App />);
+
+    expect(queryByText("Value 10")).toBeTruthy();
+  });
+
+  it("it call the increment store action in the increment button click", () => {
+    const { getByText, queryByText } = render(<App />);
+    const incrementButton = getByText("Increment");
+
+    expect(queryByText("Value 0")).toBeTruthy();
+    expect(queryByText("Value 1")).toBeFalsy();
+
+    userEvent.click(incrementButton);
+
+    expect(queryByText("Value 0")).toBeFalsy();
+    expect(queryByText("Value 1")).toBeTruthy();
+  });
+
+  it("it call the decrement store action in the decrement button click", () => {
+    const { getByText, queryByText } = render(<App />);
+    const decrementButton = getByText("Decrement");
+
+    expect(queryByText("Value 0")).toBeTruthy();
+    expect(queryByText("Value -1")).toBeFalsy();
+
+    userEvent.click(decrementButton);
+
+    expect(queryByText("Value 0")).toBeFalsy();
+    expect(queryByText("Value -1")).toBeTruthy();
   });
 });
